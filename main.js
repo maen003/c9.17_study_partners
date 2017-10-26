b5e09869464287c79f9e0edfc21fcfe233fa3df9
oowretep <13woopeter@gmail.com>
2017-10-25 20:45:01 -0700

    finished basic outline + animation/form

--- main.js
+++ main.js
@@ -16,11 +16,36 @@ function clickHandler() {
 
 function joinEvent() {
     var header = $('.joinHeader');
-    if (!header.hasClass('moveHeaderUp') || header.hasClass('.moveHeaderDown')) {
-        header.removeClass('.moveHeaderDown');
+    var findEvent = $('.findEvent');
+    if (!header.hasClass('moveHeaderUp') || header.hasClass('moveHeaderDown')) {
+        header.removeClass('moveHeaderDown');
         header.addClass('moveHeaderUp');
+
+        findEvent.addClass('animateExpandFind');
+        findEvent.removeClass('animateCloseFind');
     } else {
         header.removeClass('moveHeaderUp');
         header.addClass('moveHeaderDown')
+
+        findEvent.removeClass('animateExpandFind');
+        findEvent.addClass('animateCloseFind');
+    }
+}
+
+function createEvent() {
+    var header = $('.createHeader');
+    var createEvent = $('.createEvent');
+    if (!header.hasClass('moveHeaderUp') || header.hasClass('moveHeaderDown')) {
+        header.removeClass('moveHeaderDown');
+        header.addClass('moveHeaderUp');
+
+        createEvent.addClass('animateExpandCreate');
+        createEvent.removeClass('animateCloseCreate');
+    } else {
+        header.removeClass('moveHeaderUp');
+        header.addClass('moveHeaderDown')
+
+        createEvent.removeClass('animateExpandCreate');
+        createEvent.addClass('animateCloseCreate');
     }
 }
