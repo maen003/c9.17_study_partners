fc7a3b315570bec8f13b8d9c065df16641e92953
oowretep <13woopeter@gmail.com>
2017-10-30 12:38:48 -0700

    playing with styling, trying to make responsive

--- client/src/components/create.js
+++ client/src/components/create.js
@@ -1,86 +1,93 @@
-import React from 'react';
+import React, {Component} from 'react';
 
 import './create.css';
 
-export default () => {
-    return (
-        <div className="col-sm-6 create" id="create">
-            {/* <h1 className="createHeader">Create an Event!</h1> */}
-            <div className="createEvent">
-                <form>
-                    <div className="form-group inputArea">
-                        <div className="col-sm-6">
-                            <label for="title">Title of event</label><br/>
-                                <input type="text" id="title" className="title" placeholder="Title"/><br/>
-                        </div>
-                        <div className="col-sm-4">
-                            <label for="subjects">Event Subject</label><br/>
-                                <select id="subjects" className="subjects form-control">
-                                    <option>Life sciences</option>
-                                    <option>Visual and Perfomance Arts</option>
-                                    <option>Liberal Arts</option>
-                                    <option>Engineering and technology</option>
-                                    <option>Business</option>
-                                </select>
-                        </div>
-                        <div className="col-sm-2">
-                                <label for="groupSize">Group Size</label><br/>
-                                    <select id="groupSize" className="size form-control">
-                                        <option>2-5</option>
-                                        <option>6-10</option>
-                                        <option>11-15</option>
-                                        <option>16-20</option>
-                                        <option>21-25</option>
-                                        <option>26-30</option>
-                                        <option>> 30</option>
+class Create extends Component {
+    constructor (props) {
+        super (props);
+    }
+    render() {
+        return (
+            <div className="col-sm-6 create" id="create">
+                {/* <h1 className="createHeader">Create an Event!</h1> */}
+                <div className="createEvent">
+                    <form>
+                        <div className="form-group inputArea">
+                            <div className="col-sm-6">
+                                <label htmlFor="title">Title of event</label><br/>
+                                    <input type="text" id="title" className="title" placeholder="Title"/><br/>
+                            </div>
+                            <div className="col-sm-4">
+                                <label htmlFor="subjects">Event Subject</label><br/>
+                                    <select id="subjects" className="subjects form-control">
+                                        <option>Life sciences</option>
+                                        <option>Visual and Perfomance Arts</option>
+                                        <option>Liberal Arts</option>
+                                        <option>Engineering and technology</option>
+                                        <option>Business</option>
                                     </select>
+                            </div>
+                            <div className="col-sm-2">
+                                    <label htmlFor="groupSize">Group Size</label><br/>
+                                        <select id="groupSize" className="size form-control">
+                                            <option>2-5</option>
+                                            <option>6-10</option>
+                                            <option>11-15</option>
+                                            <option>16-20</option>
+                                            <option>21-25</option>
+                                            <option>26-30</option>
+                                            <option>> 30</option>
+                                        </select>
+                            </div>
                         </div>
-                    </div>
-                    <div className="form-group inputArea">
-                        <div className="col-sm-4 form-group">
-                            <label for="date">Date of event</label><br/>
-                                <input type="date" id="date" className="input-group date dateEvent" placeholder="MM/DD/YYYY"/>
+                        <div className="form-group inputArea">
+                            <div className="col-sm-4 form-group">
+                                <label htmlFor="date">Date of event</label><br/>
+                                    <input type="date" id="date" className="input-group date dateEvent" placeholder="MM/DD/YYYY"/>
+                            </div>
+                            <div className="col-sm-4 form-group">
+                                <label htmlFor="time">Time of event</label><br/>
+                                    <input type="time" id="time" className="time"/>
+                            </div>
+                            <div className="col-sm-4">
+                                <label htmlFor="duration">Duration of event</label><br/>
+                                    <select id="duration" className="duration form-control">
+                                        <option>Less than 1 Hour</option>
+                                        <option>1 - 2 Hours</option>
+                                        <option>2 - 3 Hours</option>
+                                        <option>3 - 4 Hours</option>
+                                        <option>4 - 5 Hours</option>
+                                        <option>> 5 Hours</option>
+                                    </select>          
+                            </div>
                         </div>
-                        <div className="col-sm-4 form-group">
-                            <label for="time">Time of event</label><br/>
-                                <input type="time" id="time" className="time"/>
+                        <div className="form-group inputArea">
+                            <div className="col-sm-5">
+                                <label htmlFor="phone">Phone Number</label><br/>
+                                    <input type="tel" id="phone" className="phone"/>
+                            </div>
+                            <div className="col-sm-5">
+                                <label htmlFor="email">Email</label><br/>
+                                    <input type="email" id="email" className="email"/>
+                            </div>
                         </div>
-                        <div className="col-sm-4">
-                            <label for="duration">Duration of event</label><br/>
-                                <select id="duration" className="duration form-control">
-                                    <option>Less than 1 Hour</option>
-                                    <option>1 - 2 Hours</option>
-                                    <option>2 - 3 Hours</option>
-                                    <option>3 - 4 Hours</option>
-                                    <option>4 - 5 Hours</option>
-                                    <option>> 5 Hours</option>
-                                </select>          
+                        <div className="form-group inputArea">
+                            <div className="col-sm-12">
+                                <label htmlFor="location">Location of event</label><br/>
+                                    <input name="location" id="location" className="location"/>
+                                    <div className="mapView"></div>
+                            </div>
                         </div>
-                    </div>
-                    <div className="form-group inputArea">
-                        <div className="col-sm-5">
-                            <label for="phone">Phone Number</label><br/>
-                                <input type="tel" id="phone" className="phone"/>
+                        <div className="form-group inputArea">
+                            <textarea className="form-group col-sm-12 description" rows="3" id="description" placeholder="Description of event..."></textarea>
                         </div>
-                        <div className="col-sm-5">
-                            <label for="email">Email</label><br/>
-                                <input type="email" id="email" className="email"/>
-                        </div>
-                    </div>
-                    <div class="form-group inputArea">
-                        <div className="col-sm-12">
-                            <label for="location">Location of event</label><br/>
-                                <input name="location" id="location" className="location"/>
-                                <div className="mapView"></div>
-                        </div>
-                    </div>
-                    <div className="form-group inputArea">
-                        <textarea className="form-group col-sm-12 description" rows="3" id="description" placeholder="Description of event..."></textarea>
-                    </div>
-                </form>
-                <button className="btn btn-danger col-sm-6" type="button">Back</button>
-                <button className="form-group btn btn-success submitForm col-sm-6" type="submit">Create Event</button>
+                    </form>
+                    <button className="btn btn-danger col-sm-6" type="button">Back</button>
+                    <button className="form-group btn btn-success submitForm col-sm-6" type="submit">Create Event</button>
+                </div>
             </div>
-        </div>
-    )
+        )
+    }
 }
+
+export default Create;
--- client/src/components/join.js
+++ client/src/components/join.js
@@ -1,38 +1,69 @@
-import React from 'react';
+import React, {Component} from 'react';
 
 import './join.css';
 
-export default () => {
-    return (
-        <div className="col-sm-6 join" id="join">
-            {/* <h1 className="joinHeader">Join an Event!</h1> */}
-            <div className="findEvent">
-                <div className="filter">
-                    <form>
-                        <div className="filterCheckbox">
-                            <label className="checkbox-inline filterSubject" for="lifeSciences">Filter Subjects
-                                <input type="checkbox" id="lifeSciences" value="Life sciences"/>
-                            </label>
-                            <label className="checkbox-inline filterSubject" for="vpArts">Visual and Perfomance Arts
-                                <input type="checkbox" id="vpArts" value="Visual and Perfomance Arts"/>
-                            </label>
-                            <label className="checkbox-inline filterSubject" for="libArts">Liberal Arts
-                                <input type="checkbox" id="libArts" value="Liberal Arts"/>
-                            </label>
-                            <label className="checkbox-inline filterSubject" for="engTech">Engineering and technology
-                                <input type="checkbox" id="engTech" value="Engineering and technology"/>
-                            </label>
-                            <label className="checkbox-inline filterSubject" for="business">Business
-                                <input type="checkbox" id="business" value="Business"/>
-                            </label>
-                        </div>
-                    </form>
-                </div>
-                <div className="map">
+class Join extends Component {
+    constructor (props) {
+        super (props);
 
+        this.state = {
+
+        }
+
+        this.clickHandler = this.clickHandler.bind(this);
+    }
+
+    clickHandler() {
+        console.log('header was clicked');
+        this.setState({
+
+        })
+    }
+
+    render() {
+        return (
+            <div className="col-sm-6 join" id="join">
+                {/* {<h1 className="joinHeader" onClick={this.clickHandler}>Join an Event!</h1>} */}
+                <div className="findEvent">
+                    <div className="map">
+
+                    </div>
+                    <div className="filter"> {/* pressing filter will animate filter options to left */}
+                        <form>
+                            <div className="formFilter">
+                                <div className="col-sm-5">    
+                                    <div className="form-group zipInput">
+                                        <input type="text" id="zipcode" className="zipcode" placeholder="Zip Code"/>
+                                    </div>
+                                    <h3>Filter By Subject</h3>
+                                    <button className="btn btn-warning" type="button">Search Again</button>
+                                </div>
+                                <div className="col-sm-7">
+                                    <div className="filterCheckbox">
+                                        <label className="checkbox-inline filterSubject" htmlFor="lifeSciences">Filter Subjects
+                                            <input type="checkbox" id="lifeSciences" value="Life sciences"/>
+                                        </label>
+                                        <label className="checkbox-inline filterSubject" htmlFor="vpArts">Visual and Perfomance Arts
+                                            <input type="checkbox" id="vpArts" value="Visual and Perfomance Arts"/>
+                                        </label>
+                                        <label className="checkbox-inline filterSubject" htmlFor="libArts">Liberal Arts
+                                            <input type="checkbox" id="libArts" value="Liberal Arts"/>
+                                        </label>
+                                        <label className="checkbox-inline filterSubject" htmlFor="engTech">Engineering and technology
+                                            <input type="checkbox" id="engTech" value="Engineering and technology"/>
+                                        </label>
+                                        <label className="checkbox-inline filterSubject" htmlFor="business">Business
+                                            <input type="checkbox" id="business" value="Business"/>
+                                        </label>
+                                    </div>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
                 </div>
-                <button className="btn btn-warning" type="button">Search Again</button>
             </div>
-        </div>
-    )
+        )
+    }
 }
+
+export default Join;
