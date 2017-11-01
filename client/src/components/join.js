a11529a303f5707b4c72739158a3cb7e4159aac0
oowretep <13woopeter@gmail.com>
2017-11-01 13:11:16 -0700

    started functionality in react

--- client/src/components/join.js
+++ client/src/components/join.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import JoinEvent from './joinEvent';
 
 import './join.css';
 
@@ -10,108 +11,56 @@ class Join extends Component {
             thing: null
         }
 
-        this.clickHandler = this.clickHandler.bind(this);
-        this.joinDiv = this.joinDiv.bind(this);
+        this.clickHandlerTrue = this.clickHandlerTrue.bind(this);
+        this.clickHandlerFalse = this.clickHandlerFalse.bind(this);
+        this.joinDivShow = this.joinDivShow.bind(this);
+        this.joinDivHide = this.joinDivHide.bind(this);
     }
 
-    clickHandler() {
+    clickHandlerTrue() {
         console.log('header was clicked');
         this.setState({
             thing: true
         })
-        // return (
-        //     <div>Sup dawg</div>
-        // )
     }
 
-    joinDiv() {
+    clickHandlerFalse() {
+        console.log('header was clicked');
+        this.setState({
+            thing: false
+        })
+    }
+
+    joinDivShow() {
         return (
-            <div>Sup dawg</div>
+            <div className="join" id="join">
+                <h1 className="joinHeader" onClick={this.clickHandlerFalse}>Join an Event!</h1>
+                <JoinEvent/>
+            </div>
+        )
+    }
+
+    joinDivHide() {
+        return (
+            <div className="join" id="join">
+                <h1 className="joinHeader" onClick={this.clickHandlerTrue}>Join an Event!</h1>
+            </div>
         )
     }
 
     render() {
         if (this.state.thing) {
-            console.log("Sup");
+            console.log(this.state.thing = true);
             return (
-                <div className="col-sm-6 col-xs-12 join" id="join">
-                    <h1 className="joinHeader">Join an Event!</h1>
-                    { this.joinDiv() }
-                    <div className="findEvent">
-                        <div className="filter"> {/* pressing filter will animate filter options to left */}
-                            <form>
-                                <div className="col-sm-5 col-xs-12 leftSideFilter">    
-                                    <div className="form-group zipInput">
-                                        <input type="text" id="zipcode" className="zipcode" placeholder="Zip Code"/>
-                                    </div>
-                                    <h3>Filter By Subject</h3>
-                                    <button className="btn btn-warning" type="button">Search Again</button>
-                                    <button className="btn btn-success" type="button">Toh gleh</button>
-                                </div>
-                                <div className="col-sm-7 col-xs-12 rightSideFilter">
-                                    <label className="checkbox-inline filterSubject" htmlFor="lifeSciences">
-                                        <input type="checkbox" id="lifeSciences" value="Life sciences"/> Filter Subjects
-                                    </label>
-                                    <label className="checkbox-inline filterSubject" htmlFor="vpArts">
-                                        <input type="checkbox" id="vpArts" value="Visual and Perfomance Arts"/> Visual and Perfomance Arts
-                                    </label>
-                                    <label className="checkbox-inline filterSubject" htmlFor="libArts">
-                                        <input type="checkbox" id="libArts" value="Liberal Arts"/> Liberal Arts
-                                    </label>
-                                    <label className="checkbox-inline filterSubject" htmlFor="engTech">
-                                        <input type="checkbox" id="engTech" value="Engineering and technology"/> Engineering and technology
-                                    </label>
-                                    <label className="checkbox-inline filterSubject" htmlFor="business">
-                                        <input type="checkbox" id="business" value="Business"/> Business
-                                    </label>
-                                </div>
-                            </form>
-                        </div>
-                        <div className="map">
-    
-                        </div>
-                    </div>
+                <div className="col-sm-6 col-xs-12 joinContainer">
+                    {this.joinDivShow()}
                 </div>
             )
         } else {
+            console.log(this.state.thing = false);
             return (
-                <div className="col-sm-6 col-xs-12 join" id="join">
-                    <h1 className="joinHeader" onClick={this.clickHandler}>Join an Event!</h1>
-                    
-                    <div className="findEvent">
-                        <div className="filter"> {/* pressing filter will animate filter options to left */}
-                            <form>
-                                <div className="col-sm-5 col-xs-12 leftSideFilter">    
-                                    <div className="form-group zipInput">
-                                        <input type="text" id="zipcode" className="zipcode" placeholder="Zip Code"/>
-                                    </div>
-                                    <h3>Filter By Subject</h3>
-                                    <button className="btn btn-warning" type="button">Search Again</button>
-                                    <button className="btn btn-success" type="button">Toh gleh</button>
-                                </div>
-                                <div className="col-sm-7 col-xs-12 rightSideFilter">
-                                    <label className="checkbox-inline filterSubject" htmlFor="lifeSciences">
-                                        <input type="checkbox" id="lifeSciences" value="Life sciences"/> Filter Subjects
-                                    </label>
-                                    <label className="checkbox-inline filterSubject" htmlFor="vpArts">
-                                        <input type="checkbox" id="vpArts" value="Visual and Perfomance Arts"/> Visual and Perfomance Arts
-                                    </label>
-                                    <label className="checkbox-inline filterSubject" htmlFor="libArts">
-                                        <input type="checkbox" id="libArts" value="Liberal Arts"/> Liberal Arts
-                                    </label>
-                                    <label className="checkbox-inline filterSubject" htmlFor="engTech">
-                                        <input type="checkbox" id="engTech" value="Engineering and technology"/> Engineering and technology
-                                    </label>
-                                    <label className="checkbox-inline filterSubject" htmlFor="business">
-                                        <input type="checkbox" id="business" value="Business"/> Business
-                                    </label>
-                                </div>
-                            </form>
-                        </div>
-                        <div className="map">
-    
-                        </div>
-                    </div>
+                <div className="col-sm-6 col-xs-12 joinContainer">
+                    {this.joinDivHide()}
                 </div>
             )
         } 
---
+++ client/src/components/joinEvent.js
@@ -0,0 +1,62 @@
+import React, {Component} from 'react';
+
+import './joinEvent.css';
+
+class JoinEvent extends Component {
+    constructor (props) {
+        super (props);
+
+        this.getDomElement = this.getDomElement.bind(this);
+    }
+
+    getDomElement() {
+        var join = document.getElementsByClassName("findEvent")[0];
+        join.className += " animateExpandFind";
+    }
+
+    componentDidMount(){
+        this.getDomElement();
+    }
+
+    render() {
+        return (
+            <div className="findEvent">
+                <div className="filter"> {/* pressing filter will animate filter options to left */}
+                    <form>
+                        <div className="col-sm-5 col-xs-12 leftSideFilter">    
+                            <div className="form-group zipInput">
+                                <input type="text" id="zipcode" className="zipcode" placeholder="Zip Code"/>
+                            </div>
+                            <h3>Filter By Subject</h3>
+                            <button className="btn btn-warning" type="button">Search Again</button>
+                            <button className="btn btn-success" type="button">Toh gleh</button>
+                        </div>
+                        <div className="col-sm-7 col-xs-12 rightSideFilter">
+                            <label className="checkbox-inline filterSubject" htmlFor="lifeSciences">
+                                <input type="checkbox" id="lifeSciences" value="Life sciences"/> Filter Subjects
+                            </label>
+                            <label className="checkbox-inline filterSubject" htmlFor="vpArts">
+                                <input type="checkbox" id="vpArts" value="Visual and Perfomance Arts"/> Visual and Perfomance Arts
+                            </label>
+                            <label className="checkbox-inline filterSubject" htmlFor="libArts">
+                                <input type="checkbox" id="libArts" value="Liberal Arts"/> Liberal Arts
+                            </label>
+                            <label className="checkbox-inline filterSubject" htmlFor="engTech">
+                                <input type="checkbox" id="engTech" value="Engineering and technology"/> Engineering and technology
+                            </label>
+                            <label className="checkbox-inline filterSubject" htmlFor="business">
+                                <input type="checkbox" id="business" value="Business"/> Business
+                            </label>
+                        </div>
+                    </form>
+                </div>
+                <div className="map">
+
+                </div>
+            </div>
+            
+        )
+    }
+}
+
+export default JoinEvent;
