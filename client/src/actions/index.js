6dd70588b99abd71769813d9efcda048a6df0740
maen003 <maen003@gmail.com>
2017-11-07 14:59:59 -0800

    removed http in path

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -2,7 +2,7 @@ import types from './types';
 import axios from 'axios';
 
 export function createEvent(form) {
-    const request = axios.post("http://dev.michaelahn.solutions/add_events", { //change to "/add_events" when pushing
+    const request = axios.post("/add_events", { //change to "/add_events" when pushing
         title: form.title,
         description: form.description,
         subject: form.subject,
@@ -22,7 +22,7 @@ export function createEvent(form) {
 }
 
 export function getAll() {
-    const request = axios.post("http://dev.michaelahn.solutions/events"); //change to "/events" when pushing
+    const request = axios.post("/events"); //change to "/events" when pushing
 
     return {
         type: types.GET_ALL,
