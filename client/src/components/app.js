7e9b212f989ee71f75adb897e594caa0bd291f67
oowretep <13woopeter@gmail.com>
2017-11-06 19:05:46 -0800

    finished modal + working on rendering event list to page

--- client/src/components/app.js
+++ client/src/components/app.js
@@ -5,13 +5,10 @@ import Nav from './home_page/nav';
 import Home from './home_page/home';
 import MainContent from './home_page/mainContent';
 
-import LoginModal from './modal/login_modal';
-
 const App = () => (
     <div className='container'>
         <Nav/>
         <Home/>
-        <LoginModal/>
         <MainContent/>
     </div>
 );
---
+++ client/src/components/joining/listEvents.js
@@ -0,0 +1,26 @@
+import React, {Component} from 'react';
+
+class EventList extends Component {
+    constructor (props) {
+        super (props);
+
+
+    }
+    render() {
+        console.log('props: ', this.props);
+        // const eventElements = this.props.allEvents.map((eventItem, index) => {
+        //     return <EventItem key={index} index={index} item={eventItem}/>
+        // });
+
+        return (
+            <div className="container">
+                <h3>All Events</h3>
+                {/* <ul>2
+                    {eventElements}
+                </ul> */}
+            </div>
+        )
+    }
+}
+
+export default EventList;
