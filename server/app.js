c0afeeec41459c527046bafe0df0cab63af9c7f6
kryseno <kryseno@gmail.com>
2017-11-10 12:28:02 -0800

    [added] routes dir, events folder. started separating routes into different files for better organization

--- server/app.js
+++ server/app.js
@@ -12,6 +12,8 @@ const FacebookStrategy = require('passport-facebook').Strategy;
 const facebookCreds = require('./facebookCreds.js');
 const session = require('express-session');
 
+const routes = require('./routes');
+
 //bodyParser
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use( bodyParser.json() );
@@ -37,87 +39,87 @@ app.use(session({
 app.use(passport.initialize());
 app.use(passport.session());
 
-
-app.get('/dbtest',function(req, res){
-    console.log(credentials);
-    const connection = mysql.createConnection(credentials);
-    connection.connect( function(){
-        console.log(arguments);
-        connection.query('SELECT * FROM events', function(err, results){
-            res.end(JSON.stringify(results));
-        })
-    })
-})
+app.use('/api', routes);
+// app.get('/dbtest',function(req, res){
+//     console.log(credentials);
+//     const connection = mysql.createConnection(credentials);
+//     connection.connect( function(){
+//         console.log(arguments);
+//         connection.query('SELECT * FROM events', function(err, results){
+//             res.end(JSON.stringify(results));
+//         })
+//     })
+// })
 
 //CREATE/JOIN EVENTS ROUTES
-app.post('/events',function(req, res){
-    const connection = mysql.createConnection(credentials);
+// app.post('/events',function(req, res){
+//     const connection = mysql.createConnection(credentials);
 
-    connection.connect(() => {
-        console.log(arguments);
-        connection.query(
-            `SELECT * FROM events`, function(err, results, fields){
-                console.log('query has finished', connection);
-                const output = {
-                    success: true,
-                    data: results
-                };
-                res.end(JSON.stringify(output));
-            });
-        console.log('query has started')
-    });
-    console.log('got a user request????');
-    //res.end('got a user request!!!!!');
-});
+//     connection.connect(() => {
+//         console.log(arguments);
+//         connection.query(
+//             `SELECT * FROM events`, function(err, results, fields){
+//                 console.log('query has finished', connection);
+//                 const output = {
+//                     success: true,
+//                     data: results
+//                 };
+//                 res.end(JSON.stringify(output));
+//             });
+//         console.log('query has started')
+//     });
+//     console.log('got a user request????');
+//     //res.end('got a user request!!!!!');
+// });
 
-app.post('/add_events',function(req, res){
-    console.log('the data is receiveth');
-    console.log('req is before this');
-    console.log("DATA RECEIVEDDDDD!!!!");
-    const connection = mysql.createConnection(credentials);
+// app.post('/add_events',function(req, res){
+//     console.log('the data is receiveth');
+//     console.log('req is before this');
+//     console.log("DATA RECEIVEDDDDD!!!!");
+//     const connection = mysql.createConnection(credentials);
 
-    const fields = `INSERT INTO events SET title = "${req.body.title}", description = "${req.body.description}", subject = "${req.body.subject}", date = "${req.body.date}", time = "${req.body.time}", duration = "${req.body.duration}", location = "${req.body.location}", max = "${req.body.max}", phone = "${req.body.phone}", email = "${req.body.email}", coordinates="123, 123", facebookID="0000"`;
-    console.log(fields);
-    console.log('this is a request body', req.body);
-    connection.connect(() => {
-        connection.query(
-            fields
-            , function(err, results, fields){
-                const output = {
-                    success: true,
-                    data: results
-                };
-                res.end(JSON.stringify(output));
-            });
-        console.log('query has started')
-    });
-    console.log('got a event request');
-    //res.end('got a user request!!!!!');
-});
+//     const fields = `INSERT INTO events SET title = "${req.body.title}", description = "${req.body.description}", subject = "${req.body.subject}", date = "${req.body.date}", time = "${req.body.time}", duration = "${req.body.duration}", location = "${req.body.location}", max = "${req.body.max}", phone = "${req.body.phone}", email = "${req.body.email}", coordinates="123, 123", facebookID="0000"`;
+//     console.log(fields);
+//     console.log('this is a request body', req.body);
+//     connection.connect(() => {
+//         connection.query(
+//             fields
+//             , function(err, results, fields){
+//                 const output = {
+//                     success: true,
+//                     data: results
+//                 };
+//                 res.end(JSON.stringify(output));
+//             });
+//         console.log('query has started')
+//     });
+//     console.log('got a event request');
+//     //res.end('got a user request!!!!!');
+// });
 
-app.post('/delete_events',function(req, res){
-    // console.log(req.body.name);
+// app.post('/delete_events',function(req, res){
+//     // console.log(req.body.name);
 
-    console.log("Data is being deleted!!!!");
-    const connection = mysql.createConnection(credentials);
+//     console.log("Data is being deleted!!!!");
+//     const connection = mysql.createConnection(credentials);
 
-    connection.connect(() => {
-        console.log(arguments);
-        console.log('this is the ',req.body.event_id);
-        connection.query(
-            `DELETE FROM events WHERE event_id = '${req.body.event_id}'`, function(err, results, fields){
-                console.log('query has finished', connection);
-                const output = {
-                    success: true,
-                    data: results
-                };
-                res.end(JSON.stringify(output));
-            });
-        console.log('query has started')
-    });
-    console.log('got a user request????');
-    //res.end('got a user request!!!!!');
-});
+//     connection.connect(() => {
+//         console.log(arguments);
+//         console.log('this is the ',req.body.event_id);
+//         connection.query(
+//             `DELETE FROM events WHERE event_id = '${req.body.event_id}'`, function(err, results, fields){
+//                 console.log('query has finished', connection);
+//                 const output = {
+//                     success: true,
+//                     data: results
+//                 };
+//                 res.end(JSON.stringify(output));
+//             });
+//         console.log('query has started')
+//     });
+//     console.log('got a user request????');
+//     //res.end('got a user request!!!!!');
+// });
 //END CREATE/JOIN EVENTS ROUTES
 
 
