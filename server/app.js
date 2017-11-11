24f3acf5fc5b5e9b7145d0b8e4d0f96f50e4eda6
maen003 <maen003@gmail.com>
2017-11-10 17:39:51 -0800

    edited server files to test for passport authenticate

--- server/app.js
+++ server/app.js
@@ -22,10 +22,9 @@ app.use( bodyParser.json() );
 app.use(cookieParser());
 
 app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", 'http://localhost:4000' );
+    res.header("Access-Control-Allow-Origin", '*' );
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     res.header('Access-Control-Allow-Credentials', true);
-    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
     next();
 });
 
