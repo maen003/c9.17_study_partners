4244d70e6aab6c443dd2b49caa46d3ecea83aebf
oowretep <13woopeter@gmail.com>
2017-11-13 14:18:42 -0800

    adding filter function based off checkbox

--- client/src/components/joining/joinEvent.js
+++ client/src/components/joining/joinEvent.js
@@ -10,10 +10,12 @@ class JoinEvent extends Component {
         super (props);
 
         this.state = {
-            eventList: null
+            eventList: null,
+            checkboxState: true
         }
 
         this.getJoinData = this.getJoinData.bind(this);
+        this.filterData = this.filterData.bind(this);
     }
 
     componentDidMount() {
@@ -26,42 +28,48 @@ class JoinEvent extends Component {
         });
     }
 
+    filterData() {
+        // console.log(this);
+        console.log('value: ', this.textInput.value);
+    }
+
     render() {
         return (
             <div className="container">
-                <div className="filter">
+                <div className="filterContainer col-sm-8 col-xs-12">
+                    <h3>Filter Results</h3>
                     <form>
-                        <div className="col-sm-5 col-xs-12 leftSideFilter">    
-                            <div className="form-group zipInput">
-                                <input type="text" id="zipcode" className="zipcode form-control" placeholder="Zip Code"/>
-                            </div>
-                            <h3>Filter By Subject</h3>
-                            <button onClick={this.getJoinData} className="btn btn-warning" type="button">Search Again</button>
-                            <button className="btn btn-success" type="button">Toh gleh</button>
+                        <div>
+                            <h4>By Subject</h4>
+                            <label className="checkbox filterCheck">
+                                <input onClick={this.filterData} type="checkbox" value="Life sciences" ref={(input) => {this.textInput = input}}/> Life Sciences
+                            </label>
+                            <label className="checkbox filterCheck">
+                                <input onClick={this.filterData} type="checkbox" value="Visual and Perfomance Arts" ref={(input) => {this.textInput = input}}/> Visual and Perfomance Arts
+                            </label>
+                            <label className="checkbox filterCheck">
+                                <input onClick={this.filterData} type="checkbox" value="Liberal Arts" ref={(input) => {this.textInput = input}}/> Liberal Arts
+                            </label>
+                            <label className="checkbox filterCheck">
+                                <input onClick={this.filterData} type="checkbox" value="Engineering and technology" ref={(input) => {this.textInput = input}}/> Engineering and technology
+                            </label>
+                            <label className="checkbox filterCheck">
+                                <input onClick={this.filterData} type="checkbox" value="Business" ref={(input) => {this.textInput = input}}/> Business
+                            </label>
                         </div>
-                        <div className="col-sm-7 col-xs-12 rightSideFilter">
-                            <label className="checkbox-inline filterSubject" htmlFor="lifeSciences">
-                                <input type="checkbox" id="lifeSciences" value="Life sciences"/> Filter Subjects
-                            </label>
-                            <label className="checkbox-inline filterSubject" htmlFor="vpArts">
-                                <input type="checkbox" id="vpArts" value="Visual and Perfomance Arts"/> Visual and Perfomance Arts
-                            </label>
-                            <label className="checkbox-inline filterSubject" htmlFor="libArts">
-                                <input type="checkbox" id="libArts" value="Liberal Arts"/> Liberal Arts
-                            </label>
-                            <label className="checkbox-inline filterSubject" htmlFor="engTech">
-                                <input type="checkbox" id="engTech" value="Engineering and technology"/> Engineering and technology
-                            </label>
-                            <label className="checkbox-inline filterSubject" htmlFor="business">
-                                <input type="checkbox" id="business" value="Business"/> Business
-                            </label>
+                        <div className="form-group zipInput">
+                            <h4>By Location</h4>
+                            <input type="text" className="zipcode form-control" placeholder="Zip Code"/>
                         </div>
                     </form>
+                    <button onClick={this.getJoinData} className="btn btn-warning" type="button">Search Again</button>
+
+                    <div className="map col-sm-12 col-xs-12">
+                        <p>map goes here</p>
+                    </div>
                 </div>
-                <div className="map">
-                    <p>map goes here</p>
-                </div>
-                <div className="list">
+                <div className="list col-sm-4 col-xs-12">
+                    <h3>All Events</h3>
                     <EventList eventList={this.props.events}/>
                 </div>
             </div>
--- client/src/reducers/event_reducer.js
+++ client/src/reducers/event_reducer.js
@@ -5,7 +5,6 @@ const DEFAULT_STATE = {all: []};
 export default (state = DEFAULT_STATE, action) => {
     switch(action.type) {
         case types.GET_ALL:
-            console.log(action.payload);
             return {
                 all: action.payload.data
             };
