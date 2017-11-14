fd61d0e835e02c5609880b6833d88a073ee20267
maen003 <maen003@gmail.com>
2017-11-14 10:32:40 -0800

    changed post to get and added user routes

--- server/routes/events.js
+++ server/routes/events.js
@@ -3,9 +3,13 @@ const mysql = require('mysql');
 const credentials = require('../mysql_credentials');
 
 const router = express.Router();
+const passport = require('passport');
+const FacebookStrategy = require('passport-facebook').Strategy;
+const facebookCreds = require('../facebookCreds.js');
+const session = require('express-session');
 
 // Reading Events
-router.post('/events',function(req, res){
+router.get('/events',function(req, res){
     const connection = mysql.createConnection(credentials);
 
     connection.connect(() => {
@@ -25,14 +29,36 @@ router.post('/events',function(req, res){
     //res.end('got a user request!!!!!');
 });
 
+router.get('/user_events',function(req, res){
+    const connection = mysql.createConnection(credentials);
+
+    connection.connect(() => {
+        console.log(arguments);
+        connection.query(
+            `SELECT * FROM events WHERE facebookID = '${req.session.passport.id}'`, function(err, results, fields){
+                console.log('query has finished', connection);
+                const output = {
+                    success: true,
+                    data: results
+                };
+                res.end(JSON.stringify(output));
+            });
+        console.log('query has started')
+    });
+    console.log('got a user request????');
+    //res.end('got a user request!!!!!');
+});
+
 // Adding Events
-router.post('/add_events',function(req, res){
-    console.log('the data is receiveth');
+router.post('/add_events',
+    passport.authenticate('facebook', { failureRedirect: '/' }),
+    function(req, res){
+        console.log('the data is receiveth');
     console.log('req is before this');
     console.log("DATA RECEIVEDDDDD!!!!");
     const connection = mysql.createConnection(credentials);
 
-    const fields = `INSERT INTO events SET title = "${req.body.title}", description = "${req.body.description}", subject = "${req.body.subject}", date = "${req.body.date}", time = "${req.body.time}", duration = "${req.body.duration}", location = "${req.body.location}", max = "${req.body.max}", phone = "${req.body.phone}", email = "${req.body.email}", coordinates="123, 123", facebookID="0000"`;
+    const fields = `INSERT INTO events SET title = "${req.body.title}", description = "${req.body.description}", subject = "${req.body.subject}", date = "${req.body.date}", time = "${req.body.time}", duration = "${req.body.duration}", location = "${req.body.location}", max = "${req.body.max}", phone = "${req.body.phone}", email = "${req.body.email}", coordinates="123, 123", facebookID=""`;
     console.log(fields);
     console.log('this is a request body', req.body);
     connection.connect(() => {
