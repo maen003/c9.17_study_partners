e11a13d59d784e9c9cc4ef0c6e07ece4dd777277
maen003 <maen003@gmail.com>
2017-11-14 12:43:54 -0800

    imported checkbox

--- client/src/components/joining/joinEvent.js
+++ client/src/components/joining/joinEvent.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import {connect} from 'react-redux';
 import {getAll} from '../../actions';
 import EventList from './listEvents';
+import Checkbox from './checkbox';
 
 import './joinEvent.css';
 
