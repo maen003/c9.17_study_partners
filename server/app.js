8987e8af62b7261bd516ee6be49c5f69ce5bb2cc
maen003 <maen003@gmail.com>
2017-11-15 18:47:22 -0800

    changing to lat and lgn

--- server/app.js
+++ server/app.js
@@ -152,8 +152,9 @@ app.post('/add_events',
         console.log('req is before this');
         console.log("DATA RECEIVEDDDDD!!!!");
         const connection = mysql.createConnection(credentials);
-        const lat = JSON.stringify()
-        const fields = `INSERT INTO events SET title = "${req.body.title}", description = "${req.body.description}", subject = "${req.body.subject}", date = "${req.body.date}", time = "${req.body.time}", duration = "${req.body.duration}", location = "${req.body.location}", max = "${req.body.max}", phone = "${req.body.phone}", email = "${req.body.email}", coordinates="${req.body.coordinates}", facebookID="${req.session.passport.user.id}"`;
+        const lat = req.body.coordinates.lat;
+        const lng = req.body.coordinates.lng;
+        const fields = `INSERT INTO events SET title = "${req.body.title}", description = "${req.body.description}", subject = "${req.body.subject}", date = "${req.body.date}", time = "${req.body.time}", duration = "${req.body.duration}", location = "${req.body.location}", max = "${req.body.max}", phone = "${req.body.phone}", email = "${req.body.email}", lat="${lat}", lng="${lng}", facebookID="${req.session.passport.user.id}"`;
         console.log(fields);
         console.log('this is a request body', req.body);
         connection.connect(() => {
