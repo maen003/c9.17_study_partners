bd08a0db263ce7fa994c69b36ed9ab5efd68ead0
pk316 <pr.khiev@gmail.com>
2017-11-20 17:37:56 -0800

    [update] updated profile page to render user profile image

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -2,7 +2,8 @@ import {combineReducers} from 'redux';
 import {reducer as formReducer} from 'redux-form';
 import eventReducer from './event_reducer';
 
+
 export default combineReducers({
     event: eventReducer,
-    form: formReducer
+    form: formReducer,
 })
