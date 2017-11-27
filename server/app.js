18198645bef65f9645f6da4278f17df696c9cdae
maen003 <maen003@gmail.com>
2017-11-27 12:40:07 -0800

    rebundle with changed subject

--- server/app.js
+++ server/app.js
@@ -181,7 +181,7 @@ app.post('/add_events',
             console.log('LOOK HERE:', req.body.coordinates);
             // Saving for later
             // lat="${req.body.coordinates.lat}", lng="${req.body.coordinates.lng}"
-            const fields = `INSERT INTO events SET title = "${req.body.title}", description = "${req.body.description}", subject = "${req.body.subject}", date = "${req.body.date}", time = "${req.body.time}", duration = "${req.body.duration}", location = "${req.body.location}", max = "${req.body.max}", phone = "${req.body.phone}", email = "${req.body.email}", coordinates = '${req.body.coordinates}', facebookID="${req.session.passport.user.id}"`;
+            const fields = `INSERT INTO events SET title = "${req.body.title}", description = "${req.body.description}", subject = "${req.body.subject}", date = "${req.body.date}", time = "${req.body.time}", duration = "${req.body.duration}", location = "${req.body.location}", max = "${req.body.max}", phone = "${req.body.phone}", email = "${req.body.email}", coordinates = '${req.body.coordinates}', facebookID="${req.session.passport.user.id}", isActive = '1'`;
             console.log(fields);
             console.log('this is a request body', req.body);
             connection.connect(() => {
