204d8dac8b0a8252bd5b470ade775d1f96b7c040
maen003 <maen003@gmail.com>
2017-11-27 19:50:35 -0800

    [edit]adding inner query

--- server/app.js
+++ server/app.js
@@ -298,25 +298,13 @@ app.post('/join_events', function (req, res){
                     if (results.RowDataPacket)
                     if (results.length<10){
                         connection.query(
-                            `SELECT * FROM joined_events WHERE facebookID = "${req.session.passport.user.id}"`, function(err, results){
+                            `INSERT INTO joined_events SET facebookID = "${req.session.passport.user.id}", event_id = "${req.body.event_id}"`, function (err, results) {
                                 const output = {
                                     success: true,
                                     data: results
                                 };
                                 res.end(JSON.stringify(output));
-                                    if (results.length == 0){
-                                        connection.query(
-                                            `INSERT INTO joined_events SET facebookID = "${req.session.passport.user.id}", event_id = "${req.body.event_id}"`, function (err, results) {
-                                                const output = {
-                                                    success: true,
-                                                    data: results
-                                                };
-                                                res.end(JSON.stringify(output));
-                                            }
-                                        )
-                                    }
                             }
-
                             // console.log("the fb id is: ", req.session.passport.user.id);
                             // console.log("The event id is: ", req.payload.data);
                         )
