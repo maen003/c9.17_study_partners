c46b8b8f194e3cedf66aed91a96cdcdd7db28f95
maen003 <maen003@gmail.com>
2017-11-29 19:02:55 -0800

    moved node mailer and prevent duplciation

--- server/app.js
+++ server/app.js
@@ -347,6 +347,36 @@ app.post('/join_events', function (req, res){
                     if (err) throw err;
                     if (results.length == 0) {
                         insertUserIntoEvent();
+                        //Start Nodemailer: Email for Event JOINED
+                        console.log('KRYSTAL: SESSION PASSPORT DATA JSON:', req.session.passport.user._json);
+                        const userEmail = req.session.passport.user._json.email;
+                        const userName = req.session.passport.user._json.first_name;
+                        const mailOptions = {
+                            from: '"Stubbies: Find Your Study Buddies!" <studies.with.stubbies@gmail.com>',
+                            to: `${userEmail}`,
+                            subject: 'Study Group Joined!',
+                            html:   `
+                <div style='background-color: white; text-align: center; font-family: tahoma'>
+                <p><img src="http://i66.tinypic.com/nzkq47.png"></p>
+                <span><i>You don't have to study lonely, with Stubbies!</i></span>
+                <hr>
+                <div style='text-align: left'>
+                    <h2>Hi, ${userName}! You have joined a study group!</h2>
+                    <p><b>${req.body.title}</b> will take place on <b>${req.body.date}</b> at <b>${req.body.time}</b>.</p>
+                    <p>If you wish to contact the group creator prior to your study session, shoot them a message at <b>${req.body.email}</b>.</p>
+                </div>
+                </div>
+                    `
+                        };
+
+                        transporter.sendMail(mailOptions, (error, info) => {
+                            if (error) {
+                                console.log('Error: ', error);
+                            } else {
+                                console.log('Email sent successfully' + info.response);
+                            }
+                        });
+                        //End Nodemailer
                     }
                     const parsedResults = JSON.parse(JSON.stringify(results));
                     const map = Array.prototype.map;
@@ -361,24 +391,15 @@ app.post('/join_events', function (req, res){
                             }
                             else if (results.length !== 0 && events.facebookID !== req.session.passport.user.id) {
                                 insertUserIntoEvent();
-                            }
-                        })
-                    }
-                    checkDuplicates();
-                }
-            )
-
-        });
-
-        //Start Nodemailer: Email for Event JOINED
-        console.log('KRYSTAL: SESSION PASSPORT DATA JSON:', req.session.passport.user._json);
-        const userEmail = req.session.passport.user._json.email;
-        const userName = req.session.passport.user._json.first_name;
-        const mailOptions = {
-            from: '"Stubbies: Find Your Study Buddies!" <studies.with.stubbies@gmail.com>',
-            to: `${userEmail}`,
-            subject: 'Study Group Joined!',
-            html:   `
+                                //Start Nodemailer: Email for Event JOINED
+                                console.log('KRYSTAL: SESSION PASSPORT DATA JSON:', req.session.passport.user._json);
+                                const userEmail = req.session.passport.user._json.email;
+                                const userName = req.session.passport.user._json.first_name;
+                                const mailOptions = {
+                                    from: '"Stubbies: Find Your Study Buddies!" <studies.with.stubbies@gmail.com>',
+                                    to: `${userEmail}`,
+                                    subject: 'Study Group Joined!',
+                                    html:   `
                 <div style='background-color: white; text-align: center; font-family: tahoma'>
                 <p><img src="http://i66.tinypic.com/nzkq47.png"></p>
                 <span><i>You don't have to study lonely, with Stubbies!</i></span>
@@ -390,16 +411,24 @@ app.post('/join_events', function (req, res){
                 </div>
                 </div>
                     `
-        };
+                                };
 
-        transporter.sendMail(mailOptions, (error, info) => {
-            if (error) {
-            console.log('Error: ', error);
-            } else {
-            console.log('Email sent successfully' + info.response);
-            }
+                                transporter.sendMail(mailOptions, (error, info) => {
+                                    if (error) {
+                                        console.log('Error: ', error);
+                                    } else {
+                                        console.log('Email sent successfully' + info.response);
+                                    }
+                                });
+                                //End Nodemailer
+                            }
+                        })
+                    }
+                    checkDuplicates();
+                }
+            )
+
         });
-        //End Nodemailer
 
     } else {
             console.log('***** ERROR: user must log into Facebook *****');
