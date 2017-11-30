dd02d19c45a05b6d9136da7e52e86b42e49517fc
maen003 <maen003@gmail.com>
2017-11-30 13:02:26 -0800

    enable people to see events without logging in[edit]

--- server/app.js
+++ server/app.js
@@ -128,7 +128,7 @@ app.get('/events',
         `;
         const queryNotLoggedIn = `SELECT events.*, events_subjects.subject AS e_s_subj
         FROM events
-        JOIN events_subjects on events.subject = events_subjects.id AND events.isActive = 1 WHERE events.facebookID != "${req.session.passport.user.id}"
+        JOIN events_subjects on events.subject = events_subjects.id AND events.isActive = 1"
         `;
         if (req.session.passport.user.id == "undefined") {
             connection.connect(() => {
