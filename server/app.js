65e6aba8ce945ae3ba6acb4ee7ec5487a14938e3
maen003 <maen003@gmail.com>
2017-11-30 17:22:31 -0800

    remoed non login

--- server/app.js
+++ server/app.js
@@ -123,11 +123,11 @@ app.get('/events',
         console.log("grumbo!!!!", req.session.passport);
         const connection = mysql.createConnection(credentials);
         console.log('The passport before undefined:', req.session.passport);
-        function loggedIn(){
-            const queryLoggedIn = `SELECT events.*, events_subjects.subject AS e_s_subj
+        const queryLoggedIn = `SELECT events.*, events_subjects.subject AS e_s_subj
                 FROM events
                 JOIN events_subjects on events.subject = events_subjects.id AND events.isActive = 1 WHERE events.facebookID != "${req.session.passport.user.id}"
                 `;
+
             connection.connect(() => {
                 connection.query(
                     queryLoggedIn, function(err, results, fields){
@@ -140,32 +140,8 @@ app.get('/events',
                     });
                 console.log('query has started')
             });
-        }
-            function notLoggedIn() {
-                const queryNotLoggedIn = `SELECT events.*, events_subjects.subject AS e_s_subj
-                FROM events
-                JOIN events_subjects on events.subject = events_subjects.id AND events.isActive = 1"
-                `;
-                connection.connect(() => {
-                    connection.query(
-                        queryNotLoggedIn, function(err, results, fields){
-                            const output = {
-                                success: true,
-                                data: results
-                            };
-                            console.log('KRYSTAL output in the the /events', output);
-                            res.end(JSON.stringify(output));
-                        });
-                    console.log('query has started')
-                });
-            }
+
         console.log('got a user request????');
-        if (req.session.passport !== undefined) {
-            loggedIn();
-        }
-        else {
-            notLoggedIn();
-        }
     });
 
 app.get('/user_events',function(req, res){
